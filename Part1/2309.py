6d4125e3d98e57f6bb51e3af2d6dcad005f55d0f
rlafl7942 <rlafl7942@naver.com>
2021-07-21 15:20:05 +0900

    2주차_study 2309 update

--- Part1/2309.py
+++ Part1/2309.py
@@ -4,12 +4,16 @@ for i in range(0,9):
     n=int(input())
     array.append(n)
     sum=sum+n
-for i in range(0,9):
+for i in range(0,8):
     for j in range(i+1,9):
         if sum-(array[i]+array[j])==100:
-            array[i]=0
-            array[j]=0
-
-for i in sorted(array):
-    if i>1:
-        print(i)
+            array[i]=-1
+            array[j]=-1
+            break
+    if array[i]==-1:
+        break
+array.sort()
+array.remove(-1)
+array.remove(-1)
+for i in array:
+    print(i)
