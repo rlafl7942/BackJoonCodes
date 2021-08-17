80522e539538fdf2b97473aebc6833e7c729fac7
rlafl7942 <rlafl7942@naver.com>
2021-08-17 12:56:12 +0900

    Solved Q3085

--- LEVEL/3085.py
+++ LEVEL/3085.py
@@ -1,39 +1,40 @@
 #틀렸습니다.
 n=int(input())
-box=[list(map(str,input())) for _ in range(n)]
-max=0
+box=[list(input()) for _ in range(n)]
+answer=1
 
 def check():
-    global max
+    global answer
     #좌우 확인
-    for i in range(0,n):
+    for i in range(n):
         cnt=1
-        for j in range(0,n-1):
+        for j in range(n-1):
             if box[i][j]==box[i][j+1]:
                 cnt+=1
-        if max<cnt:
-            max=cnt
+                answer=max(cnt,answer)
+            else:
+                cnt=1
     #위아래 확인
-    for i in range(0,n):
+    for i in range(n):
         cnt=1
-        for j in range(0,n-1):
+        for j in range(n-1):
             if box[j][i]==box[j+1][i]:
                 cnt+=1
-        if max<cnt:
-            max=cnt
+                answer=max(cnt,answer)
+            else:
+                cnt=1
+
 
 #좌우 확인 and swap
-for i in range(0,n):
-    for j in range(0,n-1):
-        if box[i][j]!=box[i][j+1]:
-            box[i][j], box[i][j+1]=box[i][j+1],box[i][j]
-            check()
-            box[i][j], box[i][j+1]=box[i][j+1],box[i][j]
+for i in range(n):
+    for j in range(n-1):
+        box[i][j], box[i][j+1]=box[i][j+1],box[i][j]
+        check()
+        box[i][j], box[i][j+1]=box[i][j+1],box[i][j]
 #위아래 확인 and swap
-for i in range(0,n):
-    for j in range(0,n-1):
-        if box[j][i]==box[j+1][i]:
-            box[j][i], box[j+1][i] = box[j+1][i], box[j][i]
-            check()
-            box[j][i], box[j+1][i] = box[j+1][i], box[j][i]
-print(max)
+for i in range(n):
+    for j in range(n-1):
+        box[j][i], box[j+1][i] = box[j+1][i], box[j][i]
+        check()
+        box[j][i], box[j+1][i] = box[j+1][i], box[j][i]
+print(answer)
