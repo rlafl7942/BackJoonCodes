340ad1df434cac9199f8d773c4fa5f787c2b161a
rlafl7942 <rlafl7942@naver.com>
2021-08-21 22:20:55 +0900

    Solved Q2231

--- LEVEL/2231.py
+++ LEVEL/2231.py
@@ -1,1 +1,14 @@
 a=int(input())
+sum=0
+for i in range(1,a+1):
+    number=i
+    sum=0
+    while number!=0:
+        sum=sum+number%10
+        number=number//10
+    if sum+i == a:
+        print(i)
+        break
+    if i==a:
+        print(0)
+        break
